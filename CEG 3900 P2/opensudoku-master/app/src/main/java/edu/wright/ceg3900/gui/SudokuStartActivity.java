43fe81d6d9ec8c9b36997dd844fe594cc0ff9442
Paul Fuchs <prf@stalbanmedia.com>
2017-02-11 18:39:33 -0500

    Upload Files

---
+++ CEG 3900 P2/opensudoku-master/app/src/main/java/edu/wright/ceg3900/gui/SudokuStartActivity.java
@@ -0,0 +1,40 @@
+package edu.wright.ceg3900.gui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import org.moire.ceg3900.R;
+
+import edu.wright.ceg3900.utils.AndroidUtils;
+
+/**
+ * Created by Paul Fuchs on 2/9/17.
+ *
+ * Provides a welcome screen which will start SudokuPlayActivity upon a button press
+ */
+
+public class SudokuStartActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AndroidUtils.setThemeFromPreferences(this);
+
+        setContentView(R.layout.sudoku_start);
+
+        Button playGme = (Button) findViewById(R.id.button_play);
+
+        playGme.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, SudokuPlayActivity.class);
+        startActivity(intent);
+    }
+}
